99684d31960e09fc803fdabd313be11f80ad745f
jacopo35 <jaczor00@gmail.com>
2021-12-09 15:12:20 +0100

    add-js structure

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*
+In allegato trovate un layout di base e un file js in cui è definito un array di oggetti che rappresentano i membro del team.
+Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+*/
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
